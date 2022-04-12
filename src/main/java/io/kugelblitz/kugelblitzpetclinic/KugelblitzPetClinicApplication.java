8e00101128a880cbc79168c81ac4387a14caa920
Zeeshan Ali Naqvi -X (zalinaqv - Altran ACT S.A.S at Cisco) <zalinaqv@cisco.com>
2022-04-12 21:02:24 +0530

    Initial commit

---
+++ src/main/java/io/kugelblitz/kugelblitzpetclinic/KugelblitzPetClinicApplication.java
@@ -0,0 +1,13 @@
+package io.kugelblitz.kugelblitzpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KugelblitzPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KugelblitzPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/kugelblitz/kugelblitzpetclinic/KugelblitzPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kugelblitz.kugelblitzpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KugelblitzPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
